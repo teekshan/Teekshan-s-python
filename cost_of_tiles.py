7777092b0c5eaa7d31aa78fc751b4a03917e4ff5
teekshan <62550959+teekshan@users.noreply.github.com>
2021-09-20 20:00:57 +0530

    create cost_of_tiles.py
    just a program to calculate ost of tiles

---
+++ cost_of_tiles.py
@@ -0,0 +1,9 @@
+def cost():
+    width = int(input("width: "))
+    height = int(input("length: "))
+    enter_cost = int(input("Enter the cost: "))
+    area_of_floor = width*height
+    total_cost = area_of_floor*enter_cost
+    print("total cost: ",total_cost)
+    
+def cost()
